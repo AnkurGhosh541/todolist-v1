23e46a9a734af66efe32bf6afd16a3000de9297c
Ankur Ghosh <ankur.ghosh.1126@gmail.com>
2022-09-13 16:34:17 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var todoItems = [];
+
+app.get("/", function (req, res) {
+  const today = new Date();
+
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  const day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", {
+    day: day,
+    todoItems: todoItems,
+  });
+});
+
+app.post("/", function (req, res) {
+  var item = req.body.newItem;
+  todoItems.push(item);
+  res.redirect("/");
+});
+
+app.listen(3000, function () {
+  console.log("server started at http://localhost:3000");
+});
