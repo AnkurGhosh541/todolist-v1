9e74d6943dbbe54ccf22177135952fcf9fce369e
Ankur Ghosh <ankur.ghosh.1126@gmail.com>
2022-09-14 19:18:40 +0530

    Change var to let

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var todoItems = [];
+let todoItems = [];
 
 app.get("/", function (req, res) {
   const today = new Date();
